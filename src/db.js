79bc11f4f2263fd33966c5dc3f56d491f97f017c
Mayank Chander <mayankchander21@gmail.com>
2019-10-19 00:24:51 +1300

    added some demo css showcasing the products, working on the categories section now

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const url = process.env.DATABASE_URL || 'mongodb://localhost:27017/shoeshop';
 
 let db;
 module.exports = {
-  connectToServer: () => mongoose.connect(url, {useNewUrlParser: true}),
+  connectToServer: () => mongoose.connect(url, {useNewUrlParser: true,  useUnifiedTopology: true }),
 
   /**
    * @return {mongoose} the database
