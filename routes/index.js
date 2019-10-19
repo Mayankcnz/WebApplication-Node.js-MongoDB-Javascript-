808814d3d2195874636ed9eed2deee631e0073fb
Luke Honiss <luke.honiss@gmail.com>
2019-10-19 18:12:32 +1300

    Remove random file, add product item partial

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const auth = require('./auth');
 const products = require('./products');
 const cart = require('./cart');
-const utils = require('../src/utils')
+const utils = require('../src/utils');
+
+const ProductController = require('../controllers/product');
 
 const router = express.Router();
 
@@ -12,7 +14,14 @@ router.use('/cart', cart);
 
 
 router.get('/', (req, res) => {
-  return utils.render(req, res, 'index', 'Home', {});
+  ProductController.findAll().then((output) => {
+    const products = output.slice(0, 6); // get 6 products
+    return utils.render(req, res, 'index', 'Home', {products: [{name: 'name', cost: 100, description: 'description', image: ''}]});
+    // return utils.render(req, res, 'index', 'Home', {products});
+  }).catch((error) => {
+    console.error(error);
+    return utils.renderError(req, res, 500, error)
+  });
 });
 
 module.exports = router;
--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const url = process.env.DATABASE_URL || 'mongodb://localhost:27017/shoeshop';
 
