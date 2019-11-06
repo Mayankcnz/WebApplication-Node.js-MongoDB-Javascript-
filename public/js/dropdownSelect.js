cea57f4334b3af1c92410de97d817bd51b4279f7
Mayank Chander <mayankchander21@gmail.com>
2019-11-06 20:59:27 +1300

    fixed overlapping css issues

--- public/js/dropdownSelect.js
+++ public/js/dropdownSelect.js
@@ -1,17 +1,17 @@
 /*Dropdown Menu*/
 $(document).ready(function() {
-$('.dropdown').click(function () {
+$('.shoedropdown').click(function () {
     $(this).attr('tabindex', 1).focus();
     $(this).toggleClass('active');
-    $(this).find('.dropdown-menu').slideToggle(300);
+    $(this).find('.shoedropdown-menu').slideToggle(300);
 });
-$('.dropdown').focusout(function () {
+$('.shoedropdown').focusout(function () {
     $(this).removeClass('active');
-    $(this).find('.dropdown-menu').slideUp(300);
+    $(this).find('.shoedropdown-menu').slideUp(300);
 });
-$('.dropdown .dropdown-menu li').click(function () {
-    $(this).parents('.dropdown').find('span').text($(this).text());
-    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+$('.shoedropdown .shoedropdown-menu li').click(function () {
+    $(this).parents('.shoedropdown').find('span').text($(this).text());
+    $(this).parents('.shoedropdown').find('input').attr('value', $(this).attr('id'));
 });
 });
 /*End Dropdown Menu*/
