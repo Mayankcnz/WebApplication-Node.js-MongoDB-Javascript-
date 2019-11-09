0ba146eba4bebbfd0b4a0a77de49bb9b502a3327
Mayank Chander <mayankchander21@gmail.com>
2019-11-09 19:58:13 +1300

    added orders page

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,14 @@
+const express = require('express');
 const order = require('../models/order');
 const router = express.Router();
+const utils = require('../src/utils')
 
 router.get('/', utils.ensureAuthenticated, (req, res) => {
 
-    console.log("Printing order");
+    order.find().then((output) =>{
+        return utils.render(req, res, 'orders', 'order', {orders: output});
+    })
+
   });
+
+  module.exports = router;
